5c8870fe73895e100e6ded07f96395501b54cad1
asif-mohmd <asifasifpsps@gmail.com>
2024-03-28 12:57:39 +0530

    forgot pass data passig perfectly

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -7,34 +7,36 @@ export class UserController {
     this.service = service;
   }
 
-
-  onForgotPassword: any = async (call:any, callback: any) =>{
-    try{
-      console.log("contoll forgot password")
+  onForgotPassword: any = async (call: any, callback: any) => {
+    try {
+      console.log("contoll forgot password");
 
       const request = call.request as {
         email: string;
         password: string;
+      };
+
+      const response = await this.service.forgotPassword(
+        request.email,
+        request.password
+      );
+      console.log("----------", response);
+      console.log("555555555", response.activatonToken);
+      if (response.forgotPasswordStatus) {
+        console.log(response.activationToken, "===========================");
+        callback(null, {
+          registerStatus: true,
+          forgotData: response.activationToken,
+        });
+      } else {
+        callback(null, {
+          registerStatus: false,
+        });
       }
-
-      const response = await this.service.forgotPassword(request.email, request.password)
-      console.log("----------",response)
-console.log("555555555", response.activatonToken)
-      callback(null,{
-        forgotPasswordStatus : response,
-        forgotData : response.activatonToken
-      })
-
-
-
-    }catch(error:any){
-      callback(error); 
+    } catch (error: any) {
+      callback(error);
     }
-  }
-
-
-
-
+  };
 
   onRegister: any = async (call: any, callback: any) => {
     try {
@@ -45,88 +47,69 @@ console.log("555555555", response.activatonToken)
         password: string;
       };
       const response = await this.service.userRegister(request);
-      console.log( "-----------------------------",response);
-      if(response.registerStatus){
-        console.log(response.activationToken,"===========================")
+      console.log("-----------registerrr resss------------------", response);
+      if (response.registerStatus) {
+        console.log(response.activationToken, "===========================");
         callback(null, {
-            msg: "OTP send",
-            registerStatus: true,
-            userData :response.activationToken
-         
-           
-          });
-      }else{
+          msg: "OTP send",
+          registerStatus: true,
+          userData: response.activationToken,
+        });
+      } else {
         callback(null, {
-            msg: "Auth error",
-            registerStatus: false,
-           
-          });
+          msg: "Auth error",
+          registerStatus: false,
+        });
       }
-     
     } catch (error) {
-    
       callback(error);
     }
   };
 
-  onActivateUser: any = async (call:any, callback:any) =>{
-    try{
-        console.log("on activate",call.request,"-----------")
+  onActivateUser: any = async (call: any, callback: any) => {
+    try {
+      console.log("on activate", call.request, "-----------");
 
-        const request = call.request as {
-            name:string;
-            email:string;
-            password:string;
-        }
-        const response = await this.service.activateUser(request);
-        console.log(response,"-----------------")
-        if(response){
-          console.group("res sett")
-          callback(null,{
-            status:true
-          })
-        }else{
-          console.log("222222222222222")
-          callback(null,{
-            status:false
-          }) 
-        }
-       
-
-    }catch(err){
-        callback(err);
+      const request = call.request as {
+        name: string;
+        email: string;
+        password: string;
+      };
+      const response = await this.service.activateUser(request);
+      console.log(response, "-----------------");
+      if (response) {
+        console.group("res sett");
+        callback(null, {
+          status: true,
+        });
+      } else {
+        console.log("222222222222222");
+        callback(null, {
+          status: false,
+        });
+      }
+    } catch (err) {
+      callback(err);
     }
-  }
-
-
-
+  };
 
   onLogin: any = async (call: any, callback: any) => {
     try {
       console.log("controller call");
-      const {email,password} = call.request as {
+      const { email, password } = call.request as {
         email: string;
         password: string;
       };
-      const response = await this.service.userLogin(email,password);
-  
-      callback(null, {
-        
-        msg: response.msg,
-        status: response.status, 
-        otp:response.otp,
-        loginStatus:response.loginStatus
-     
+      const response = await this.service.userLogin(email, password);
 
+      callback(null, {
+        msg: response.msg,
+        status: response.status,
+        otp: response.otp,
+        loginStatus: response.loginStatus,
       });
     } catch (error) {
-     
-      callback(error); 
+      callback(error);
     }
   };
-
-  
-
-
-  
 }
